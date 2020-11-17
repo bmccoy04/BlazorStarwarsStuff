0335767ed95afd9aafa3e4871c34c5a6d627f793
bryan <bryanmccoy04@gmail.com>
2020-11-16 21:10:23 -0600

    c# 9 features

--- src/StarwarsStuff.Con/Program.cs
+++ src/StarwarsStuff.Con/Program.cs
@@ -1,19 +1,63 @@
-﻿using System;
+﻿using static System.Console;
 
-namespace StarwarsStuff.Con
+PrintName();
+
+static void PrintName()
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var p = new Person(){Name ="Bryan"};
-            p.Name = "James";
-            Console.WriteLine("Hello: " + p.Name);
-        }
-    }
+    var p = new Person { Name = "Bryan" };
 
-    record Person
+    Pet petOne = new Dog { Name = "Jack" };
+
+    var petTwo = petOne with
     {
-        public string Name { get; set; }
+        Name = "Sally"
+    };
+
+    var petThree = petOne with
+    {
+        Name = "Jack"
+    };
+
+    var a = new Animal("Cat");
+
+    var someAnimal = new Something("Cat", "Dog");
+
+    var (cat, dog) = someAnimal;
+
+    //p.Name = "James"; doesn't work now
+
+    WriteLine("Hello: " + p.Name);
+
+    WriteLine(petTwo);
+
+    WriteLine(Equals(petOne, petThree));
+}
+
+
+class Person
+{
+    public string Name { get; init; }
+} 
+
+// a class that has value like behavior
+record Animal(string Type);
+
+record Something(string Type, string Name) : Animal(Type);
+
+record Pet
+{ 
+    public string Name { get; init; }
+
+    public Person Owner { get; init; }
+
+    public bool GetName()
+    {
+        return true; 
     }
+}
+
+
+record Dog : Pet
+{
+    public bool Barks { get; set; }
 }
